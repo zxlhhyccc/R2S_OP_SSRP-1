3c6eb3542460b9e0e41b91c6f7888105717fd3c9
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2020-02-21 20:39:50 +0800

    Update Stage1.sh

--- Stage1.sh
+++ Stage1.sh
@@ -13,6 +13,8 @@ popd
 latest_release="$(curl -s https://github.com/openwrt/openwrt/releases |grep -Eo "v[0-9\.]+.tar.gz" |sed -n 1p)"
 curl -LO "https://github.com/openwrt/openwrt/archive/${latest_release}"
 tar zxvf ${latest_release}  --strip-components 1 -C ./friendlywrt-rk3328/friendlywrt
+rm -f ./friendlywrt-rk3328/friendlywrt/feeds.conf.default
+wget -P friendlywrt-rk3328/friendlywrt/ https://raw.githubusercontent.com/openwrt/openwrt/openwrt-19.07/feeds.conf.default
 rm -f ${latest_release}
 cp -r ./R2S_PATCH/. ./friendlywrt-rk3328/friendlywrt
 cd friendlywrt-rk3328
