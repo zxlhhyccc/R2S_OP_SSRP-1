731ce5f13f3cfeed108eba7609f7a7933363fad4
QiuSimons <QiuSimons@users.noreply.github.com>
2020-02-25 18:16:33 +0800

    #6

--- R2S_PATCH/target/linux/sunxi/image/cortex-a53.mk
+++ R2S_PATCH/target/linux/sunxi/image/cortex-a53.mk
@@ -5,8 +5,9 @@
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
+ifeq ($(SUBTARGET),cortexa53)
 
-define Device/friendlyarm_nanopi-neo-plus2
+define Device/sun50i-h5-nanopi-neo-plus2
   DEVICE_VENDOR := FriendlyARM
   DEVICE_MODEL := NanoPi NEO Plus2
   DEVICE_TITLE:=Nanopi NEO Plus2 (H5)
@@ -14,60 +15,62 @@ define Device/friendlyarm_nanopi-neo-plus2
 	kmod-leds-gpio kmod-ledtrig-heartbeat \
 	kmod-brcmfmac brcmfmac-firmware-43430-sdio wpad-mini
   SUPPORTED_DEVICES:=nanopi-neo-plus2
-  SUNXI_DTS_DIR := allwinner/
-  SOC := sun50i-h5
+  SUNXI_DTS:=allwinner/sun50i-h5-nanopi-neo-plus2
   KERNEL_NAME := Image
   KERNEL := kernel-bin
 endef
-TARGET_DEVICES += friendlyarm_nanopi-neo-plus2
 
-define Device/friendlyarm_nanopi-neo2
-  DEVICE_VENDOR := FriendlyARM
-  DEVICE_MODEL := NanoPi NEO2
+TARGET_DEVICES += sun50i-h5-nanopi-neo-plus2
+
+define Device/sun50i-h5-nanopi-neo2
+  DEVICE_TITLE:=Nanopi NEO2 (H5)
   SUPPORTED_DEVICES:=nanopi-neo2
-  SUNXI_DTS_DIR := allwinner/
-  SOC := sun50i-h5
+  SUNXI_DTS:=allwinner/sun50i-h5-nanopi-neo2
   KERNEL_NAME := Image
   KERNEL := kernel-bin
 endef
-TARGET_DEVICES += friendlyarm_nanopi-neo2
 
-define Device/pine64_pine64-plus
-  DEVICE_VENDOR := Pine64
-  DEVICE_MODEL := Pine64+
-  SUNXI_DTS_DIR := allwinner/
-  SOC := sun50i-a64
-  KERNEL_NAME := Image
-  KERNEL := kernel-bin
-endef
-TARGET_DEVICES += pine64_pine64-plus
+TARGET_DEVICES += sun50i-h5-nanopi-neo2
 
-define Device/pine64_sopine-baseboard
-  DEVICE_VENDOR := Pine64
-  DEVICE_MODEL := SoPine
-  SUNXI_DTS_DIR := allwinner/
-  SOC := sun50i-a64
+define Device/sun50i-a64-pine64-plus
+  DEVICE_TITLE:=Pine64 Plus A64
+  SUPPORTED_DEVICES:=pine64,pine64-plus
+  SUNXI_DTS:=allwinner/sun50i-a64-pine64-plus
   KERNEL_NAME := Image
   KERNEL := kernel-bin
 endef
-TARGET_DEVICES += pine64_sopine-baseboard
 
-define Device/xunlong_orangepi-pc2
-  DEVICE_VENDOR := Xunlong
-  DEVICE_MODEL := Orange Pi PC 2
-  SUNXI_DTS_DIR := allwinner/
-  SOC := sun50i-h5
-  KERNEL_NAME := Image
-  KERNEL := kernel-bin
-endef
-TARGET_DEVICES += xunlong_orangepi-pc2
+TARGET_DEVICES += sun50i-a64-pine64-plus
 
-define Device/xunlong_orangepi-zero-plus
-  DEVICE_VENDOR := Xunlong
-  DEVICE_MODEL := Orange Pi Zero Plus
-  SUNXI_DTS_DIR := allwinner/
-  SOC := sun50i-h5
+define Device/sun50i-a64-sopine-baseboard
+  DEVICE_TITLE:=Pine64 Sopine
+  SUPPORTED_DEVICES:=pine64,sopine-baseboard
+  SUNXI_DTS:=allwinner/sun50i-a64-sopine-baseboard
   KERNEL_NAME := Image
   KERNEL := kernel-bin
 endef
-TARGET_DEVICES += xunlong_orangepi-zero-plus
+
+TARGET_DEVICES += sun50i-a64-sopine-baseboard
+
+
+define Device/sun50i-h5-orangepi-zero-plus
+  DEVICE_TITLE:=Xunlong Orange Pi Zero Plus
+  SUPPORTED_DEVICES:=xunlong,orangepi-zero-plus
+  SUNXI_DTS:=allwinner/sun50i-h5-orangepi-zero-plus
+  KERNEL_NAME := Image
+  KERNEL := kernel-bin
+endef
+
+TARGET_DEVICES += sun50i-h5-orangepi-zero-plus
+
+define Device/sun50i-h5-orangepi-pc2
+  DEVICE_TITLE:=Xunlong Orange Pi PC2
+  SUPPORTED_DEVICES:=xunlong,orangepi-pc2
+  SUNXI_DTS:=allwinner/sun50i-h5-orangepi-pc2
+  KERNEL_NAME := Image
+  KERNEL := kernel-bin
+endef
+
+TARGET_DEVICES += sun50i-h5-orangepi-pc2
+
+endif
